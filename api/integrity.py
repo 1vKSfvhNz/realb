2e21643ae74d1391b6aa2d6b4fe3526c8a143eef
Ozias Kévin Jairus BELEMSOBGO <noomwende4@gmail.com>
2025-04-24 23:27:40 +0000

    Initial commit

--- api/integrity.py
+++ api/integrity.py
@@ -1,12 +1,15 @@
-import hashlib
-from fastapi import APIRouter, Response
+from os import getenv
 from typing import Dict
+from fastapi import APIRouter, Response
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Créer un routeur pour l'intégrité du bundle
 bundle_integrity = APIRouter()
 
 # Stocker le hash correct quelque part (peut être dans une base de données, variable d'environnement ou en dur pour la simplicité)
-EXPECTED_BUNDLE_HASH = "votre_hash_de_bundle_précalculé_ici"  # À mettre à jour à chaque nouvelle version
+EXPECTED_BUNDLE_HASH = getenv('EXPECTED_BUNDLE_HASH')  # À mettre à jour à chaque nouvelle version
 
 @bundle_integrity.get("/bundle-hash", response_model=Dict[str, str])
 async def get_bundle_hash():
